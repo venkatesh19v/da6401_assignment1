7c52bb2aa9a30cb173acb8d29f2ce37fe441bf1d
venkatesh19v <venkateshvenky1982002@gmail.com>
2025-03-08 18:46:01 +0530

    feat: activation function as a Module

---
+++ activation.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def relu(x):
+    return np.maximum(0, x)
+
+def relu_derivative(x):
+    return (x > 0).astype(float)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    sig = sigmoid(x)
+    return sig * (1 - sig)
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    return 1 - np.tanh(x)**2
+
+def softmax(x):
+    ex_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return ex_x / np.sum(ex_x, axis=1, keepdims=True)
