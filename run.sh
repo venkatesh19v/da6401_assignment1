91a57c84bcec99085479e55e99e9f1d1b35a8073
venkatesh19v <venkateshvenky1982002@gmail.com>
2025-03-17 00:38:55 +0530

    fix: run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-echo "Running Test 1: Minimal training run with default parameters..."
-python3 train.py --epochs 15 --weight_init xavier --weight_decay 0 --batch_size 8 --optimizer nadam --learning_rate 0.0001 --activation relu --num_layers 4 --hidden_size 256
+echo "Running Test 1: Minimal training run with best parameters..."
+python3 train.py --epochs 20 --weight_init random --weight_decay 0.5 --batch_size 64 --optimizer adam --learning_rate 0.001 --activation relu --num_layers 3 --hidden_size 256
 
-echo "Running Test 2: Minimal training run with default parameters..."
-python3 train.py --epochs 15 --weight_init xavier --weight_decay 0.0005 --batch_size 32 --optimizer adam --learning_rate 0.0001 --activation tanh --num_layers 4 --hidden_size 256
-# relu	32	0.9	20	0.000001	3	256	784	0.0001	0.95		nadam	10	0.0005	xavier
+echo "Running Test 2: Minimal training run with best parameters..."
+python3 train.py --epochs 20 --weight_init xavier --weight_decay 0.0005 --batch_size 64 --optimizer rmsprop --learning_rate 0.001 --activation relu --num_layers 7 --hidden_size 256
+
+echo "Running Test 3: Minimal training run with best parameters..."
+python3 train.py --epochs 20 --weight_init xavier --weight_decay 0.0005 --batch_size 8 --optimizer nadam --learning_rate 0.0001 --activation relu --num_layers 7 --hidden_size 256
+
+echo "Running Test 4: Minimal training run with best parameters for MSE..."
+python3 train.py --epochs 10 --weight_init xavier --weight_decay 0.0005 --batch_size 8 --optimizer nadam --learning_rate 0.0001 --activation relu --num_layers 5 --hidden_size 256 --loss mean_squared_error
+
+echo "Running Test 5: Minimal training run with best parameters for MSE..."
+python3 train.py --epochs 15 --weight_init xavier --weight_decay 0.0005 --batch_size 8 --optimizer nadam --learning_rate 0.0001 --activation relu --num_layers 6 --hidden_size 256 --loss mean_squared_error
+
+echo "Running Test 6: Minimal training run with best parameters for MSE..."
+python3 train.py --epochs 20 --weight_init xavier --weight_decay 0.0005 --batch_size 8 --optimizer nadam --learning_rate 0.0001 --activation tanh --num_layers 7 --hidden_size 256 --loss mean_squared_error
+
+echo "Running Test 7: Minimal training run with best parameters on mnist dataset ..."
+python3 train.py --epochs 20 --weight_init random --weight_decay 0.5 --batch_size 64 --optimizer adam --learning_rate 0.001 --activation relu --num_layers 3 --hidden_size 256 --dataset mnist
+
+echo "Running Test 8: Minimal training run with best parameters on mnist dataset for MSE..."
+python3 train.py --epochs 20 --weight_init random --weight_decay 0.5 --batch_size 64 --optimizer adam --learning_rate 0.001 --activation relu --num_layers 3 --hidden_size 256 --dataset mnist --loss mean_squared_error
